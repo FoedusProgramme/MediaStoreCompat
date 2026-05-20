ef63c678b128b1c73aed18e0573b6146de665177
nift4 <nift4@protonmail.com>
2026-05-20 19:08:33 +0200

    change artifact id

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -72,7 +72,7 @@ mavenPublishing {
     publishToMavenCentral()
     signAllPublications()
 
-    coordinates("io.github.nift4.mediastorecompat", "minsdk21", "1.0.0-alpha12")
+    coordinates("io.github.nift4.mediastorecompat", "mediastorecompat", "1.0.0-alpha12")
 
     pom {
         name.set("MediaStoreCompat")
